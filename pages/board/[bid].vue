96794fa91d475982e3e745451b261e22a860b10f
milu0925u <milu17y@gmail.com>
2025-02-10 08:27:14 +0800

    dd

--- pages/board/[bid].vue
+++ pages/board/[bid].vue
@@ -1,18 +1,21 @@
 <template>
     <div class="container">
         <div class="content">
-            <div class="title">{{title}}</div>
+            <div class="title">{{ title }}</div>
             <Editer ref="deltaContent" :isViewing="isViewing" />
         </div>
-        <div class="content-reply"> 
-            <!-- <div class="reply" v-for="m in allmessage">
-                <div class="content-message"><b>{{m && getUserById(m.uid).username}}</b><p>{{m && m.content}}</p><div class="createdate">{{m && m.createdate}}</div></div>
-         </div> -->
-         <!-- <div class="text-btn">
+        <div class="content-reply">
+            <div class="reply" v-for="m in allmessage">
+                <div class="content-message"><b>{{ m && getUserById(m.uid).username }}</b>
+                    <p>{{ m && m.content }}</p>
+                    <div class="createdate">{{ m && m.createdate }}</div>
+                </div>
+            </div>
+            <div class="text-btn">
                 <input type="text" v-model="message" />
                 <button class="btn" @click="sendMessage">傳送</button>
-            </div> -->
-        </div>    
+            </div>
+        </div>
         <div class="btn-group">
             <button class="btn" @click="goToHome">返回</button>
         </div>
@@ -35,27 +38,27 @@ const deltaContent = ref(null); //  存放 Delta 格式內容
 const isViewing = ref(true); // 編輯狀態
 
 const fetchData = async (bid) => {
-        try {
-            const response = await axios.get(`/api/board/${bid}`);
-            if (response.data.success) {
-                title.value = response.data.data.title;
-                deltaContent.value?.setEditorContent(response.data.data.content)
-            }
-        } catch (error) {
-            toast.error(response.data.message)
-        };
-    }
-    const fetchReplyData = async (bid) => {
-        try {
-            const response = await axios.get(`/api/boardreply/${bid}`);
-            if (response.data.success) {
-                allmessage.value = response.data.data
-                await nextTick();
-            }
-        } catch (error) {
-            toast.error(error)
-        };
-    }
+    try {
+        const response = await axios.get(`/api/board/${bid}`);
+        if (response.data.success) {
+            title.value = response.data.data.title;
+            deltaContent.value?.setEditorContent(response.data.data.content)
+        }
+    } catch (error) {
+        toast.error(response.data.message)
+    };
+}
+const fetchReplyData = async (bid) => {
+    try {
+        const response = await axios.get(`/api/boardreply/${bid}`);
+        if (response.data.success) {
+            allmessage.value = response.data.data
+            await nextTick();
+        }
+    } catch (error) {
+        toast.error(error)
+    };
+}
 // 回首頁
 const goToHome = () => {
     navigateTo('/')
@@ -64,31 +67,31 @@ const goToHome = () => {
 // 留言
 const message = ref('');
 const allmessage = ref([]);
-const sendMessage = async()=>{
+const sendMessage = async () => {
     const bid = route.params.bid; // 文章編號
     // 假設未登入
-    if (!user.value){
+    if (!user.value) {
         await $swal.fire({
-        title: "您尚未登入，是否前往登入?",
-        showCancelButton: true,
-        confirmButtonText: "登入",
-        cancelButtonText: "取消",
+            title: "您尚未登入，是否前往登入?",
+            showCancelButton: true,
+            confirmButtonText: "登入",
+            cancelButtonText: "取消",
         }).then((result) => {
-        if (result.isConfirmed) {
-            navigateTo('/login')
-        } 
-        });
-    return
-    }
-        try {
-            const response = await axios.post(`/api/boardreply/${bid}`,{type:'add',uid:user.value.id,content:message.value});
-            if (response.data.success) {
-                allmessage.value = response.data.data
+            if (result.isConfirmed) {
+                navigateTo('/login')
             }
-        } catch (error) {
-            console.log(error)
-            
-        };
+        });
+        return
+    }
+    try {
+        const response = await axios.post(`/api/boardreply/${bid}`, { type: 'add', uid: user.value.id, content: message.value });
+        if (response.data.success) {
+            allmessage.value = response.data.data
+        }
+    } catch (error) {
+        console.log(error)
+
+    };
 
 }
 
@@ -100,7 +103,7 @@ const getUserById = (uid) => {
 // 監聽路由變更
 watch(() => [route.params.bid], (newBid) => {
     if (newBid) {
-        fetchData(newBid) 
+        fetchData(newBid)
         fetchReplyData(newBid)
     };
 }, { immediate: true });
@@ -110,117 +113,138 @@ watch(() => [route.params.bid], (newBid) => {
 </script>
 
 <style lang="scss" scoped>
-.container{
-    --background:rgba(161, 132, 119, 1);
-    --title:rgba(105, 82, 72, 1);
-    --text:rgba(253, 233, 224, 1);
-    --border:rgba(86, 38, 16, 1);
-    margin-inline:auto;
-    background:var(--background);
-    display:flex;
-    padding:24px;
+.container {
+    --background: rgba(161, 132, 119, 1);
+    --title: rgba(105, 82, 72, 1);
+    --text: rgba(253, 233, 224, 1);
+    --border: rgba(86, 38, 16, 1);
+    margin-inline: auto;
+    background: var(--background);
+    display: flex;
+    padding: 24px;
     position: relative;
-    .content{
-        width:75%;
-        background:#FCFCFC;
-        background:var(--text);
-        >div{
+
+    .content {
+        width: 75%;
+        background: #FCFCFC;
+        background: var(--text);
+
+        >div {
             border-style: solid;
             border-color: var(--border);
-            &:first-child{
+
+            &:first-child {
                 border-width: 2px 2px 0 2px;
             }
-            &:last-child{
-            border-width: 0 2px 2px 2px;
+
+            &:last-child {
+                border-width: 0 2px 2px 2px;
             }
         }
-        .text-btn{
-            margin-top:auto;
+
+        .text-btn {
+            margin-top: auto;
         }
-    };
-    .content-reply{
-        flex:1;
-        display:flex;
+    }
+
+    ;
+
+    .content-reply {
+        flex: 1;
+        display: flex;
         flex-direction: column;
         justify-content: space-between;
-        padding-left:16px;
-        input[type="text"]{
-            width:100%;
-            padding:8px 8px;
-            margin-block:4px;
-            border:1px solid var(--border);
+        padding-left: 16px;
+
+        input[type="text"] {
+            width: 100%;
+            padding: 8px 8px;
+            margin-block: 4px;
+            border: 1px solid var(--border);
         }
-        button{
+
+        button {
             // background:var(--title);
         }
-        .reply{
-        display:flex;
-        flex-direction: column;
-        gap:6px;
-        font-size:12px; 
-        margin-top:80px;
-            >div{
-            border:1px solid var(--border);
-            border-radius: 8px;
-            width:100%;
-            padding:12px 16px;
-            background:#FCFCFC;
+
+        .reply {
+            display: flex;
+            flex-direction: column;
+            gap: 6px;
+            font-size: 12px;
+            margin-top: 80px;
+
+            >div {
+                border: 1px solid var(--border);
+                border-radius: 8px;
+                width: 100%;
+                padding: 12px 16px;
+                background: #FCFCFC;
             }
         }
     }
-    
-    .title{
-    display: flex;
-    justify-content: space-between;
-    padding: 36px 12px;
-    height:50px;
-    color:white;
-    align-items: center;
-    background:	var(--title);
+
+    .title {
+        display: flex;
+        justify-content: space-between;
+        padding: 36px 12px;
+        height: 50px;
+        color: white;
+        align-items: center;
+        background: var(--title);
     }
-    .btn-group{
-    position: absolute;
-    display:flex;
-    gap:16px;
-    button{
-    width:80px;
-    height:25px;
-    }
-    top:24px;
-    right:24px;
+
+    .btn-group {
+        position: absolute;
+        display: flex;
+        gap: 16px;
+
+        button {
+            width: 80px;
+            height: 25px;
+        }
+
+        top:24px;
+        right:24px;
     }
 }
 
-.content-message{
-    display:flex;
+.content-message {
+    display: flex;
     flex-direction: column;
-    div{
-    margin-left:auto;}
+
+    div {
+        margin-left: auto;
+    }
 }
 
 @media screen and (max-width: 768px) {
-    .container{
-        width:100%;
-        margin:0;
-        padding-top:4rem;
+    .container {
+        width: 100%;
+        margin: 0;
+        padding-top: 4rem;
         flex-direction: column;
-        .content{
-            width:100%;
-            margin-bottom:16px;
+
+        .content {
+            width: 100%;
+            margin-bottom: 16px;
         }
-        .content-reply{
-            padding:0;
+
+        .content-reply {
+            padding: 0;
         }
-        .btn-group{
-            height:50px;
-            top:16px;
-           right:26px;
-            button{
-            font-size:12px;
-            width:50px;
-            height:24px;
+
+        .btn-group {
+            height: 50px;
+            top: 16px;
+            right: 26px;
+
+            button {
+                font-size: 12px;
+                width: 50px;
+                height: 24px;
             }
-    }
+        }
     }
 }
 </style>
