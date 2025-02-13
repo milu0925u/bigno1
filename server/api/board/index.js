10e02540758c498c7bec1f88a6f20026bf4ab0c3
milu0925u <milu17y@gmail.com>
2025-02-13 11:26:51 +0800

    ww

--- server/api/board/index.js
+++ server/api/board/index.js
@@ -136,136 +136,130 @@ import moment from "moment";
 
 
 
-// export default async function handler(req) {
-//   await connectToDatabase(); 
+export default async function handler(req) {
+  await connectToDatabase(); 
 
   
-//     if (req.method === "GET") {
-//     const boards = await Board.find().sort({ createdate: -1 }).lean();
+    if (req.method === "GET") {
+    const boards = await Board.find().sort({ createdate: -1 }).lean();
 
-//     const formattedBoards = boards.map(b => ({
-//       ...b,
-//       createdate: moment(b.createdate).format('YYYY-MM-DD'),
-//       updatedate: b.updatedate ? moment(b.updatedate).format('YYYY-MM-DD') : null,
-//       hiddendate: b.hiddendate ? moment(b.hiddendate).format('YYYY-MM-DD') : null
-//     }))
+    const formattedBoards = boards.map(b => ({
+      ...b,
+      createdate: moment(b.createdate).format('YYYY-MM-DD'),
+      updatedate: b.updatedate ? moment(b.updatedate).format('YYYY-MM-DD') : null,
+      hiddendate: b.hiddendate ? moment(b.hiddendate).format('YYYY-MM-DD') : null
+    }))
 
-//     return new Response(JSON.stringify({
-//       success: true,
-//       message: '取得成功',
-//       data:formattedBoards,
-//     }), {
-//       status: 200,
-//     });
-//   }
+    return new Response(JSON.stringify({
+      success: true,
+      message: '取得成功',
+      data:formattedBoards,
+    }), {
+      status: 200,
+    });
+  }
 
-//     if (req.method === "PUT") {
-//       const body = await req.text();  // 使用 req.text() 獲取原始請求體
-//       const parsedData = JSON.parse(body);  // 將請求體解析為 JSON
+    if (req.method === "PUT") {
+      const body = await req.text();  // 使用 req.text() 獲取原始請求體
+      const parsedData = JSON.parse(body);  // 將請求體解析為 JSON
 
-//       const { uid, bid, type, title, jsondata } = parsedData;
-
-
-
-//       // const parsedData = await req.json();  // 使用 req.json() 來解析 JSON 請求體
-//       // const { uid, bid, type, title, jsondata } = parsedData;
+      const { uid, bid, type, title, jsondata } = parsedData;
 
 
 
+      // const parsedData = await req.json();  // 使用 req.json() 來解析 JSON 請求體
+      // const { uid, bid, type, title, jsondata } = parsedData;
 
-//       // const body = await req.text();
-//       // let parsedData;
-//       // try {
-//       //   parsedData = JSON.parse(body);
-//       // } catch (error) {
-//       //   return new Response(JSON.stringify({ success: false, message: '無效的 JSON 格式' }), {
-//       //     status: 400,
-//       //   });
-//       // }
-//       // const { uid, bid, type,title, jsondata } = parsedData;
-//     if (type === "addboard"){
-//     // 抓到最後一筆編號
-//     let lastNum = await Board.findOne().sort({ bid: -1 }).limit(1);
-//     const bnewid = lastNum ? Number(lastNum.bid) + 1 : 1;
 
-//     // 抓到今天日期
-//     const today = new Date().toISOString().split('T')[0]
 
-//     const newBoard = new Board({
-//       bid:bnewid,
-//       uid:uid,
-//       title:title,
-//       content:jsondata,
-//       createdate: today ,
-//     });
-//     await newBoard.save();
-//     return new Response(JSON.stringify({
-//       success: true,
-//       message: "發布成功！",
-//     }), {
-//       status: 200,
-//     });
-//     }else if (type ==="updateboard"){
-//       console.log('我有夾帶資料進入');
+
+      // const body = await req.text();
+      // let parsedData;
+      // try {
+      //   parsedData = JSON.parse(body);
+      // } catch (error) {
+      //   return new Response(JSON.stringify({ success: false, message: '無效的 JSON 格式' }), {
+      //     status: 400,
+      //   });
+      // }
+      // const { uid, bid, type,title, jsondata } = parsedData;
+    if (type === "addboard"){
+    // 抓到最後一筆編號
+    let lastNum = await Board.findOne().sort({ bid: -1 }).limit(1);
+    const bnewid = lastNum ? Number(lastNum.bid) + 1 : 1;
+
+    // 抓到今天日期
+    const today = new Date().toISOString().split('T')[0]
+
+    const newBoard = new Board({
+      bid:bnewid,
+      uid:uid,
+      title:title,
+      content:jsondata,
+      createdate: today ,
+    });
+    await newBoard.save();
+    return new Response(JSON.stringify({
+      success: true,
+      message: "發布成功！",
+    }), {
+      status: 200,
+    });
+    }else if (type ==="updateboard"){
+      console.log('我有夾帶資料進入');
       
-//       // 解析base64
-//       const updatedBoard = await Board.findOneAndUpdate(
-//         { bid: bid },
-//         { 
-//           $set: { 
-//             uid: uid,
-//             title: title,
-//             content: jsondata,
-//             updatedate: new Date()
-//           }
-//         },
-//         { new: true } 
-//       );
+      // 解析base64
+      const updatedBoard = await Board.findOneAndUpdate(
+        { bid: bid },
+        { 
+          $set: { 
+            uid: uid,
+            title: title,
+            content: jsondata,
+            updatedate: new Date()
+          }
+        },
+        { new: true } 
+      );
     
-//       return new Response(JSON.stringify({
-//           success: true,
-//           message: '更新成功！',
-//           data:updatedBoard,
-//         }));
-//     }
-//   };
+      return new Response(JSON.stringify({
+          success: true,
+          message: '更新成功！',
+          data:updatedBoard,
+        }));
+    }
+  };
 
-//   if (req.method === "PATCH") {
-//     // const body = await req.text();
-//     //   let parsedData;
-//     //   try {
-//     //     parsedData = JSON.parse(body);
-//     //   } catch (error) {
-//     //     return new Response(JSON.stringify({ success: false, message: '無效的 JSON 格式' }), {
-//     //       status: 400,
-//     //     });
-//     //   }
-//     //   const { uid,bid,type,state } = parsedData;
-
-
-//     // const parsedData = await req.json();  // 使用 req.json() 來解析 JSON 請求體
-//     // const  { uid,bid,type,state } = parsedData;
+  if (req.method === "PATCH") {
+    // const body = await req.text();
+    //   let parsedData;
+    //   try {
+    //     parsedData = JSON.parse(body);
+    //   } catch (error) {
+    //     return new Response(JSON.stringify({ success: false, message: '無效的 JSON 格式' }), {
+    //       status: 400,
+    //     });
+    //   }
+    //   const { uid,bid,type,state } = parsedData;
 
 
-//            if (type === "statechange"){
-//             if (!state){
-//               await  Board.updateOne({ bid: bid },  { $set: { hiddendate: null } } );
-//             }else {
-//               const now = new Date().toISOString().split("T")[0] 
-//               await  Board.updateOne({ bid: bid }, { $set: { hiddendate:  now } } );
-//             }
-//           return new Response(JSON.stringify({
-//             success: true,
-//             message: '變更成功！',
-//             data:updatedBoard,
-//           }));
-//         }}
-// }
+    // const parsedData = await req.json();  // 使用 req.json() 來解析 JSON 請求體
+    // const  { uid,bid,type,state } = parsedData;
 
 
-export const config = {
-  runtime: 'edge', // 使用 Edge Function
-};
-export default function handler(req, res) {
-  res.status(200).json({  success: true,message: "Hello from Vercel Edge!" });
+           if (type === "statechange"){
+            if (!state){
+              await  Board.updateOne({ bid: bid },  { $set: { hiddendate: null } } );
+            }else {
+              const now = new Date().toISOString().split("T")[0] 
+              await  Board.updateOne({ bid: bid }, { $set: { hiddendate:  now } } );
+            }
+          return new Response(JSON.stringify({
+            success: true,
+            message: '變更成功！',
+            data:updatedBoard,
+          }));
+        }}
 }
+
+
