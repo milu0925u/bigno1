b208a631f4faef3db6a3a771442ef02a99d14251
milu0925u <milu17y@gmail.com>
2025-02-13 10:18:12 +0800

    console.log('11完成');

--- server/api/board/index.js
+++ server/api/board/index.js
@@ -31,6 +31,8 @@ export default defineEventHandler(async (event) => {
   // 使用formdata
   if (event.req.method === "POST") {
     const form = new IncomingForm({  maxFileSize: 8 * 1024 * 1024});
+    console.log('11完成');
+    
     const formData = await new Promise((resolve, reject) => {
       form.parse(event.node.req, (err, fields, files) => {
           if (err) {
@@ -39,10 +41,12 @@ export default defineEventHandler(async (event) => {
           resolve(fields); // 只有成功時才會執行
       });
     });
+
+    console.log('22完成');
     const jsonString = formData.jsondata?.[0] || "";  // 取出字串
     const {type,uid,title,jsondata} = JSON.parse(jsonString); // 解析 JSON
     const newjson =  LZString.decompressFromBase64(jsondata)
-
+    console.log('33完成');
     if (type === "addboard"){
     // 抓到最後一筆編號
     let lastNum = await Board.findOne().sort({ bid: -1 }).limit(1);
